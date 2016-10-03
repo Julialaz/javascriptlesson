bed9d7df13c44e1e86d19419b4f3e6c9a9ca4675
Julia Lazurkevych <lazurkevich@yahoo.com>
2016-10-03 12:29:54 +0000

    add new file

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+alert(max(10, -20));
+function sum(a, b, c) {
+return Math.sqrt(a + b + c); 
+}
+
+function max (a, b) {
+    if (a>b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+var a = 2;
+var b = 5;
+var c = 2;
 
 //var name = prompt('Input your name');
 //var hello ="Hello"+ name;
@@ -15,15 +31,3 @@
 //var b = Number (prompt('Input your month of birth'));
 //var c = Number (prompt('Input your date of birth'));
 //document.writeln(sum(a+b+c));
-
-function sum(a,b,c) {
-return Math.sqrt(a+b+c) 
-}
-
-function max (a,b) {
-    if (a>b) {
-        return a;
-    } else {
-        return b;
-    }
-}
---
+++ indexhome.js
@@ -0,0 +1,5 @@
+var name = prompt('Input your name');
+var hello ="Hello"+ name;
+document.writeln(hello);
+alert(hello);
+console.log(hello);
