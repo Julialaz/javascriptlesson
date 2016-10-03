f9ca1fe14ee7be30d6b24bd5fed32539be30d1fb
Julia Lazurkevych <lazurkevich@yahoo.com>
2016-10-03 17:22:28 +0000

    add new file

---
+++ example.js
@@ -0,0 +1,14 @@
+alert(max (10, -20));
+function sum(a, b, c) {
+return Math.sqrt(a + b + c); 
+}
+
+function max (a, b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
